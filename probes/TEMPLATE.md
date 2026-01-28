a395d5256b6779e203adaf10f5f42bb9a0969a7f
Tomas Zajic <tomiz87dev@gmail.com>
2026-01-28 10:20:58 +0100

    docs: add more comments to the zgp-1 code and template for probes readme (#3)

---
+++ probes/TEMPLATE.md
@@ -0,0 +1,64 @@
+<h1>ZGP-X</h1>
+<p>
+<b>Name:</b> ZGP-X (+ alias or nickname)<br>
+<b>Developed in:</b> 20XX (Year or Date)<br>
+<b>Status</b>: Planned/In development/Testing/Finished<br><br>
+<b>Main Component:</b> ...<br>
+<b>Power Source:</b> USB (external/host-powered) / 9V Battery / Solar Panel...<br>
+<b>Measuring:</b> ...<br>
+</p>
+
+Probe summary in a few sentences<br><br>
+
+<table align="center">
+  <thead>
+    <tr>
+      <th markdown><a href="./src/main.ino">Code</a></th>
+      <th markdown><a href="">Video</a></th>
+    </tr>
+  </thead>
+</table>
+
+<p align="center" width="100%">
+  <img src="./logo.png" alt="ZGP-X Logo" height="250" width="250">
+</p>
+
+<h2>Components</h2>
+<ul>
+    <li>1x Main Component</li>
+    <li>1x Breadboard</li>
+    <li>1x ...</li>
+    <li>1x ...</li>
+    <li>Cables/Wires</li>
+    <li>USB-B to USB-A cable</li>
+</ul>
+
+<h3>Schema</h3>
+<p align="center" width="100%">
+  <img src="./schema.png" alt="ZGP-X Schema" height="350"><br><br>
+</p>
+
+<h4>Pin Mapping</h4>
+<table align="center">
+    <tr><th>Component</th><th>Interface</th></tr>
+    <tr><td>...</td><td>Pin1, Pin2</td></tr>
+    <tr><td>...</td><td>Pin1, Pin2</td></tr>
+    <tr><td>...</td><td>Pin1, Pin2</td></tr>
+    <tr><td>...</td><td>Pin1, Pin2</td></tr>
+</table>
+
+<h2>Requirements</h2>
+<h3>Functional Requirements:</h3>
+<ol>
+    <li>...(✅ if fulfilled)</li>
+</ol>
+
+Other Requirement types may be applicable (NFR, Constraints...)
+
+<h2>Testing</h2>
+...
+
+<h2>Recommendations for the next probe</h2>
+<ul>
+    <li>...</li>
+</ul>
--- probes/ZGP-01/src/main.ino
+++ probes/ZGP-01/src/main.ino
@@ -1,15 +1,19 @@
+/*
+  ZGP-1 Source Code
+  by Tomas & Tibor Zajic (2020)
+*/
+
 #include <Wire.h>
 #include <SFE_BMP180.h>
-
-SFE_BMP180 bmp180;
-int Altitude = 5; //current altitude in meters
-
 #include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+SFE_BMP180 bmp180;
+
+int Altitude = 5; // Current altitude in meters
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //Mapping pins
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
 void setup() {
+  // Start of Serial Monitor
   Serial.begin(9600);
   bool success = bmp180.begin();
 
@@ -17,21 +21,21 @@ void setup() {
     Serial.println("BMP180 init success");
   }
 
-  // set up the LCD's number of columns and rows:
+  // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
+
+  // Print a message to the LCD.- First row
   lcd.print("ZGP-01");
-  
 }
 
 void loop() {
-
   char status;
-  double T, P;
+  double T, P; // temperature and pressure (pressure is unused while printing)
   bool success = false;
 
   status = bmp180.startTemperature();
 
+  // Getting temperature and checks
   if (status != 0) {
     delay(1000);
     status = bmp180.getTemperature(T);
@@ -46,6 +50,7 @@ void loop() {
         if (status != 0) {
           float comp = bmp180.sealevel(P, Altitude);
           
+          // Prints the temperature into the Serial monitor - for testing
           Serial.print("Temperature: ");
           Serial.print(T);
           Serial.println(" C");
@@ -53,18 +58,17 @@ void loop() {
       }
     }
   }
+
+  // Prints time since powering up on the first row after "ZGP-01"
   lcd.setCursor(8, 0);
   lcd.print(" >");
-  lcd.print(millis()/1000);
+  lcd.print(millis() / 1000);
     
+  // Second row printing (temperature)
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-
   lcd.print("T = ");
   lcd.print(T);
   lcd.print("");
   lcd.print((char)223);
   lcd.print("C");
-
-
 }

7986a526b6448f85695e8bb3972245c157728787
Tomas Zajic <tomiz87dev@gmail.com>
2026-02-07 23:30:12 +0100

    docs: add comments to the zgp-2 code (#4)

--- probes/ZGP-01/src/main.ino
+++ probes/ZGP-01/src/main.ino
@@ -24,7 +24,7 @@ void setup() {
   // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 
-  // Print a message to the LCD.- First row
+  // Print a message to the LCD - First row
   lcd.print("ZGP-01");
 }
 
--- probes/ZGP-02-Polaris/src/main.ino
+++ probes/ZGP-02-Polaris/src/main.ino
@@ -1,50 +1,59 @@
+/*
+  ZGP-02 Source Code
+  by Tomas & Tibor Zajic (2021)
+*/
+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
-#define DHTPIN 2    // modify to the pin we connected
-#define DHTTYPE DHT11   // DHT 11 (AM2301)
+
+#define DHTPIN 2      // Pin Used - for mapping
+#define DHTTYPE DHT11 // DHT 11 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27,16,2);
-void setup()
-{
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  // Start and setup the LCD
   lcd.init(); 
   lcd.backlight(); 
 
+  // Start of Serial Monitor
   Serial.begin(9600);
-  delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
+  delay(500);
 
-  // set up the LCD's number of columns and rows:
-  lcd.begin(20, 2);
-  // Print a message to the LCD.
+  // Wait before accessing Sensor
+  Serial.println("Waiting for DHT11\n\n");
+  delay(1000);
+
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Print a message to the LCD - First row
   lcd.print("ZGP-02");
 }
-void loop() 
-{
+
+void loop() {
+  // Reading Temperature and humidity from DHT11 Sensor
   int t, h;
-  
-  lcd.setCursor(1,0); 
   t = dht.readTemperature();
   h = dht.readHumidity();
-
+  
+  // Prints time since powering up on the first row after "ZGP-02"
   lcd.setCursor(7, 0);
   lcd.print(" >");
   lcd.print(millis()/1000);
     
+  // Second row printing (temperature data)
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-
   lcd.print("t:");
   lcd.print(t);
   lcd.print("");
   lcd.print((char)223);
   lcd.print("C ");
 
-    lcd.setCursor(9, 1);
-  // print the number of seconds since reset:
-
+  // Second row printing (humidity data)
+  lcd.setCursor(9, 1);
   lcd.print("h:");
   lcd.print(h);
   lcd.print(" %");
